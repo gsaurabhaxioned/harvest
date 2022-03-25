cc1691b817784d926efb2577b81bcdd97aa3e417
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-25 15:35:55 +0530

    js code adde for hamburger and filtersearch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-let category = document.querySelector(".all-job-category");
-console.log(category.innerHTML);
+$(document).ready(function(){
+    $(".ham").click(function(){
+        $(this).toggleClass("cross");
+        $(".menus").slideToggle();
+    })
+})
+
+let category = document.querySelector(".all-job-category"),
+category_content = document.querySelectorAll(".category-content");
+category.addEventListener("change",()=>{
+    let category_name = category.value.toLowerCase();
+    category_content.forEach(i=>{
+        if(i.innerHTML.toLowerCase().indexOf(category_name) > -1) {
+            i.classList.remove("hidecontent");
+        }
+    })
+})
 
 
 
