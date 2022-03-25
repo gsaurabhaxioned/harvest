45a044359804b07046cd82b1643180eed94cabe9
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-25 15:48:48 +0530

    js code completed for filter search categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,8 @@ category_content = document.querySelectorAll(".category-content");
 category.addEventListener("change",()=>{
     let category_name = category.value.toLowerCase();
     category_content.forEach(i=>{
+        i.classList.add("hidecontent");
+        i.classList.remove("category-content");
         if(i.innerHTML.toLowerCase().indexOf(category_name) > -1) {
             i.classList.remove("hidecontent");
         }
